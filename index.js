f9527e14e5871b054fc99c3ab902834aa3d5b125
Khyati Rahul <khyatirahul@Khyatis-MacBook-Air.local>
2024-02-13 13:24:51 -0500

    adding edit delete

--- index.js
+++ index.js
@@ -20,31 +20,58 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
     Blog.find({}).then((allBlogs)=>{
-        res.render('index.ejs', {blogs: allBlogs});
-    });
+        res.render('index.ejs', {blogs: allBlogs})
+    })
 });
 
 app.get('/write', (req,res) => {
-    res.render('write.ejs');
+    res.render('write.ejs')
 });
 
 app.get('/read', (req,res) => {
     Blog.find({}).then((allBlogs)=>{
-        res.render('read.ejs', {blogs: allBlogs});
-    });
+        res.render('read.ejs', {blogs: allBlogs})
+    })
 });
 
 app.post('/write', (req,res) => {
     var blog = new Blog({
         title: req.body.title,
         content: req.body.content
-    });
+    })
 
     blog.save().then(() =>{
         Blog.find({}).then((allBlogs)=>{
             res.render('read.ejs', {blogs: allBlogs});
+        }).catch((err)=>{
+            console.log(err);
+        })
+    })
+});
+
+app.post('/edit', (req, res) => {
+    if(req.body.edit){
+        Blog.findById(req.body.edit).then((blog)=>{
+            res.render('write.ejs', {blog: blog})
         });
+    }
+    else if(req.body.delete){
+        Blog.findByIdAndDelete(req.body.delete).then(() =>{
+            res.redirect('/read')
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+});
+
+app.post('/update', (req, res) =>{
+    console.log(req.body);
+    Blog.findByIdAndUpdate(req.body.id, {title: req.body.title, content: req.body.content}).then(()=>{
+        res.redirect('/read')
+    }).catch((err)=>{
+        console.log(err);
     });
+
 });
 
 app.listen(port, (err)=>{
